3bab06e12da710a1cfcb159ef0bf519fac8208c3
sz376 <sz376@njit.edu>
2021-12-03 15:36:27 -0500

    began friends integration

---
+++ app/src/main/java/com/example/venu/models/Friend.java
@@ -0,0 +1,28 @@
+package com.example.venu.models;
+
+import com.parse.ParseObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Friend {
+    String name;
+    String friendPhoto;
+
+    public Friend(ParseObject friend) {
+        name = friend.get("Name").toString();
+        friendPhoto = friend.getParseFile("Image").getUrl();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getfriendPhotoUrl() {
+        return friendPhoto;
+    }
+    
+}
